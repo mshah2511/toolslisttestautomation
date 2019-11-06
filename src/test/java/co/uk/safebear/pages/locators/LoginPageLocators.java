ac3288c3f9419919d06c94c9b8b663980d32dfbe
student <student@safebear.co.uk>
2019-11-06 12:23:55 +0000

    add page locators

---
+++ src/test/java/co/uk/safebear/pages/locators/LoginPageLocators.java
@@ -0,0 +1,18 @@
+package co.uk.safebear.pages.locators;
+
+import lombok.Data;
+import org.openqa.selenium.By;
+
+@Data
+public class LoginPageLocators {
+
+    //fields
+    private By usernameLocator = By.id("username");
+    private By passwordLocator = By.id("password");
+
+    //buttons
+    private By loginbuttonLocator = By.id("enter");
+
+    //messages
+    private By failedLoginMessage = By.xpath(".//p[@id=\"rejectLogin\"]/b");
+}
