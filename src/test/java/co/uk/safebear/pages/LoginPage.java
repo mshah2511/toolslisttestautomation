1409d90a424a81837ac29f431489cca6429c6258
student <student@safebear.co.uk>
2019-11-06 14:18:27 +0000

    loginpage locators

--- src/test/java/co/uk/safebear/pages/LoginPage.java
+++ src/test/java/co/uk/safebear/pages/LoginPage.java
@@ -1,4 +1,36 @@
 package co.uk.safebear.pages;
 
+import co.uk.safebear.pages.locators.LoginPageLocators;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.WebDriver;
+
+@RequiredArgsConstructor
 public class LoginPage {
+
+    LoginPageLocators locators = new LoginPageLocators();
+
+    @NonNull
+    WebDriver driver;
+
+   public String getPageTitle(){
+
+       return driver.getTitle();
+   }
+
+   public void enterUsername (String username){
+
+       driver.findElement(locators.getUsernameLocator()).sendKeys(username);
+   }
+
+   public void enterPassword (String password) {
+       driver.findElement(locators.getPasswordLocator()).sendKeys(password);
+   }
+
+   public void clickLoginButton(){
+       driver.findElement(locators.getLoginbuttonLocator()).click();
+   }
+
+
 }
+
--- src/test/java/co/uk/safebear/pages/ToolsPage.java
+++ src/test/java/co/uk/safebear/pages/ToolsPage.java
@@ -1,4 +1,15 @@
 package co.uk.safebear.pages;
 
+import co.uk.safebear.pages.locators.ToolsPageLocators;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.WebDriver;
+
+@RequiredArgsConstructor
 public class ToolsPage {
+
+    ToolsPageLocators locators = new ToolsPageLocators();
+
+    @NonNull
+    WebDriver driver;
 }
